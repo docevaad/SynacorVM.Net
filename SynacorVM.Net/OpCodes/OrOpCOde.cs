4bedc38814830c6c43de07e32e31fd782e9a5cd0
David Tomcik <tomcik.david@gmail.com>
2017-05-17 11:48:49 -0700

    Additional cleanup removing uneeded imports.

--- SynacorVM.Net/OpCodes/OrOpCOde.cs
+++ SynacorVM.Net/OpCodes/OrOpCOde.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SynacorVM.Net.OpCodes
+﻿namespace SynacorVM.Net.OpCodes
 {
     public class OrOpCode : ISVMOpCode
     {
